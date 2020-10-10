85b57346a559f5a383468bfa88fc2bb80e4b5bfc
vicky-hemnani <58837151+vicky-hemnani@users.noreply.github.com>
2020-10-10 19:05:23 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+ <footer class="foo">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p style="text-align:center; font-family: 'Monotype Corsiva'; font-size:17px;"><i class="material-icons" style="color: brown;">VICKY</i> 2020 <b >Online Notes Sharing </b></p>
+                </div>
+            </div>
+   </footer>
